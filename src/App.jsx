7e1d14ac47cce4662f20af1165772809f604e3f3
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-03-21 20:33:56 -0500

    comment added

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 function App() {
   const [list, setList] = useState(null);
-
+  
+  // Step 1: Query the CryptoCompare API to get a list of all the coins
   // make our first API call and use that to display a list of crypto coins in our react app
   // use the useEffect() hook to call our API to guarantee that our API call is only used when the page renders, and allow the browser to display the screen even if all
   // the information isn't quite ready yet making for a better user experience.
@@ -22,7 +23,7 @@ function App() {
   }, []);
 
   console.log(list.Data)
-  
+
   // When it comes time to render our actual list of coins, using .map(), we first want to check that our API is not still waiting on results to fill up our list, and 
   // once we know list is not empty, we can display it.
   // Here we are using an if statement to filter down some of our coins so that we are only seeing blockchain coins. This makes our page a little more efficient
